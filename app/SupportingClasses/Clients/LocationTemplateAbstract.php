08b81d0d460d3b03ed5d0898dee60f466c81f5de
Denys Kozedub <kozedubdenis@gmail.com>
2020-02-03 17:25:23 +0000

    last 1

---
+++ app/SupportingClasses/Clients/LocationTemplateAbstract.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\SupportingClasses\Clients;
+
+
+ abstract class LocationTemplateAbstract
+{
+     abstract protected function locationCreator(string $modelName, string $name, string $needle) :string;
+
+     //Rules: key - name of models
+     //Rules: val - name which will be add to model
+     //Rules: needle - which value returns model
+     //Result ['country_id' => 1]
+
+     public function resultCreator(array $params) : array
+     {
+         $data = [];
+         foreach ($params as $key => $val) {
+             $data[$key.'_id'] = $this->locationCreator($key,$val,'id');
+         }
+         return $data;
+     }
+
+}
