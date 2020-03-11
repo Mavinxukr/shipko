c1ebd8299768c3d1b58f1950bc3988f9af138d1d
Yaroslav <yroslav005@gmail.com>
2020-03-11 10:43:59 +0200

    Fix response

--- app/Traits/FormattedJsonResponse.php
+++ app/Traits/FormattedJsonResponse.php
@@ -7,11 +7,25 @@ use Illuminate\Http\JsonResponse;
 
 trait FormattedJsonResponse
 {
-    public function toJson(string $message, int $statusCode, object $resource = null)
+    /*public function toJson(string $message, int $statusCode, object $resource = null)
     {
         return !is_null($resource) ?
                                       $resource->response()->setStatusCode($statusCode,$message):
                                       response()->json(['message'=> $message], $statusCode);
+    }*/
+
+    public function toJson(string $message, int $statusCode, object $resource = null)
+    {
+        $status = false;
+        if($statusCode == 200)
+            $status = true;
+
+        return response()->json([
+            'message'   => $message,
+            'status'    => $status,
+            'code'      => $statusCode,
+            'data'      => $resource,
+        ], $statusCode);
     }
 }
 
