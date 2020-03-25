7d3c8777c97980fb4c6f1cc654905d4141fb027b
Yaroslav <yroslav005@gmail.com>
2020-03-25 18:31:35 +0200

    Fix

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -17,19 +17,20 @@ class ClientResource extends Resource
         $full_address = $country . " " . $city . " " . $address . " " . $zip;
 
         return  [
-            'id'            =>  $this->id,
-            'name'          =>  $this->name,
-            'username'      =>  $this->username,
-            'email'         =>  $this->email,
-            'phone'         =>  $this->phone,
-            'card_number'   =>  $this->card_number,
-            'country'       =>  $this->country->name ?? null,
-            'city'          =>  $this->city->name ?? null,
-            'zip'           =>  $this->zip->name ?? null,
-            'address'       =>  $this->address->name ?? null,
-            'image'         =>  !is_null($this->image) ? $this->image->path_to_front : null,
-            'date_register' =>  $this->created_at->format('d.m.Y'),
-            'full_address'  =>  $full_address,
+            'id'                =>  $this->id,
+            'name'              =>  $this->name,
+            'username'          =>  $this->username,
+            'email'             =>  $this->email,
+            'phone'             =>  $this->phone,
+            'card_number'       =>  $this->card_number,
+            'country'           =>  $this->country->name ?? null,
+            'city'              =>  $this->city->name ?? null,
+            'zip'               =>  $this->zip->name ?? null,
+            'address'           =>  $this->address->name ?? null,
+            'image'             =>  !is_null($this->image) ? $this->image->path_to_front : null,
+            'date_register'     =>  $this->created_at->format('d.m.Y'),
+            'full_address'      =>  $full_address,
+            'new_notification'  =>  $this->notifications()->where('is_new', 1)->count(),
             ];
     }
 
