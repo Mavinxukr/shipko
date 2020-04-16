20c7846e1815db543fbe305aa164985bdbe7a1af
Yaroslav <yroslav005@gmail.com>
2020-04-16 12:04:52 +0300

    Fix

--- app/Traits/Service/AutoService/AutoFilterCollection.php
+++ app/Traits/Service/AutoService/AutoFilterCollection.php
@@ -9,8 +9,34 @@ trait AutoFilterCollection
 {
     public function getAutoWithFilter($model)
     {
+        $filters = [
+            'auto_year'         => 'year',
+            'auto_make'         => 'make_name',
+            'auto_model'        => 'model_name',
+            'shipping_status'   => [
+                'relationship'  => 'shipping',
+                'field'         => 'status'
+            ],
+            'port'              => [
+                'relationship'  => 'shipInfo',
+                'field'         => 'point_load_city'
+            ],
+        ];
+
+        $data = \request()->all();
+        foreach ($data as $k => $item){
+            if(is_array($filters[$k])){
+                $model->whereHas($filters[$k]['relationship'],
+                    function (Builder $query) use ($filters, $k, $item){
+                        $query->where($filters[$k]['relationship'], $item);
+                    });
+            }else{
+                $model->where($filters[$k], $item);
+            }
+        }
+
         $search = \request('search');
-        $status = \request('shipping_status');
+        /*$status = \request('shipping_status');
         $year = \request('auto_year');
         $makeName = \request('auto_make');
         $modelName = \request('auto_name');
@@ -39,7 +65,7 @@ trait AutoFilterCollection
             $model->whereHas('shipInfo', function (Builder $query) use ($port){
                 $query->where('point_load_city', $port);
             });
-        }
+        }*/
 
         if(!is_null($search)){
             $model->whereHas('lotInfo', function (Builder $autoQuery) use ($search) {
@@ -57,6 +83,36 @@ trait AutoFilterCollection
         $data['models'] = Auto::select('model_name')->distinct()->get();
 
         return $data;
+    }
+
+    public function filter()
+    {
+        $filters = [
+            'auto_year'         => 'year',
+            'auto_make'         => 'make_name',
+            'auto_model'        => 'model_name',
+            'shipping_status'   => [
+                'relationship'  => 'shipping',
+                'field'         => 'status'
+            ],
+            'port'              => [
+                'relationship'  => 'shipInfo',
+                'field'         => 'point_load_city'
+            ],
+        ];
+
+        $data = \request()->all();
+        foreach ($data as $k => $item){
+            if(is_array($filters[$k])){
+                $model->whereHas($filters[$k]['relationship'],
+                    function (Builder $query) use ($filters, $k, $item){
+                        $query->where($filters[$k]['relationship'], $item);
+                });
+            }else{
+                $model->where($filters[$k], $item);
+            }
+        }
+
     }
 }
 
