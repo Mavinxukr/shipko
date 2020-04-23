2ad046d5520bd68947d628388bee02937080e478
Yaroslav <yroslav005@gmail.com>
2020-04-23 10:15:12 +0300

    Fix

--- app/Http/Resources/PriceResource.php
+++ app/Http/Resources/PriceResource.php
@@ -27,7 +27,7 @@ class PriceResource extends JsonResource
             'priceable_type'    => Price::morphMap('type', $this->priceable_type),
             'priceable_id'      => $this->priceable_id,
             'priceable'         => $priceable,
-            'cities'            => $this->cities()->get('name', 'id'),
+            'cities'            => $this->cities()->select('id', 'name')->get(),
         ];
     }
 }
