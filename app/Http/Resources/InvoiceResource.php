ffe67568460f0d556de36f6e706d70088ff652b2
Yaroslav <yroslav005@gmail.com>
2020-05-05 15:45:32 +0300

    FIx

--- app/Http/Resources/InvoiceResource.php
+++ app/Http/Resources/InvoiceResource.php
@@ -19,8 +19,10 @@ class InvoiceResource extends JsonResource
 
         if($this->auto->offsite){
             $totalPrice = $this->total_price + $this->auto->offsite_price;
+            $outstandingPrice = $this->outstanding_price + $this->auto->offsite_price;
         }else{
             $totalPrice = $this->total_price;
+            $outstandingPrice = $this->outstanding_price;
         }
 
         return [
@@ -29,11 +31,11 @@ class InvoiceResource extends JsonResource
             'paiment_for'               => ['Auction invoice', 'Shipping charge invoice'],
             'total'                     => [$totalPrice, $this->total_shipping_price],
             'paid'                      => [$this->paid_price,$this->paid_shipping_price],
-            'outstanding'               => [$this->outstanding_price, $this->outstanding_shipping_price],
+            'outstanding'               => [$outstandingPrice, $this->outstanding_shipping_price],
             'status'                    => [$this->status, $this->status_shipping],
             'total_price'               => $totalPrice,
             'paid_price'                => $this->paid_price,
-            'outstanding_price'         => $this->outstanding_price,
+            'outstanding_price'         => $outstandingPrice,
             'total_shipping_price'      => $this->total_shipping_price,
             'paid_shipping_price'       => $this->paid_shipping_price,
             'outstanding_shipping_price'=> $this->outstanding_shipping_price,
