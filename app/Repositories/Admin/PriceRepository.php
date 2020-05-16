0015dcad09730cebd38915046c10ce2ad973d2b2
Yaroslav <yroslav005@gmail.com>
2020-05-16 19:40:08 +0300

    Fix

--- app/Repositories/Admin/PriceRepository.php
+++ app/Repositories/Admin/PriceRepository.php
@@ -58,7 +58,7 @@ class PriceRepository implements PriceContract
 
     public function update(Request $request, int $id)
     {
-        $data = array_filter($request->only('name'));
+        $data = array_filter($request->only('name', 'country_id'));
         try {
             if(!is_null($request->priceable_type) && !is_null($request->priceable_id)){
                 $data['priceable_id'] = $request->priceable_id;
