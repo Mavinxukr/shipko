41265a92facf2229d8399fe508a079fea0872cd1
Yaroslav <yroslav005@gmail.com>
2020-07-20 11:17:34 +0300

    Fix

--- database/seeds/AutoTableTest.php
+++ database/seeds/AutoTableTest.php
@@ -11,12 +11,32 @@ class AutoTableTest extends Seeder
      */
     public function run()
     {
-        \App\Models\Auto\Auto::create([
-            'year'          => '2014',
-            'make_name'     => 'Mercedes',
-            'model_name'    => 'Mercedes',
-            'client_id'     => 1,
-            'purchased_date'=> \Carbon\Carbon::now()->format('Y-m-d'),
-        ]);
+        $autos = [
+            [
+                'year'          => '2014',
+                'make_name'     => 'Mercedes',
+                'model_name'    => 'Mercedes',
+                'client_id'     => 1,
+                'purchased_date'=> \Carbon\Carbon::now()->format('Y-m-d'),
+            ],
+            [
+                'year'          => '2015',
+                'make_name'     => 'Mercedes',
+                'model_name'    => 'Mercedes',
+                'client_id'     => 2,
+                'purchased_date'=> \Carbon\Carbon::now()->format('Y-m-d'),
+            ],
+            [
+                'year'          => '2016',
+                'make_name'     => 'Mercedes',
+                'model_name'    => 'Mercedes',
+                'client_id'     => 3,
+                'purchased_date'=> \Carbon\Carbon::now()->format('Y-m-d'),
+            ]
+        ];
+
+        foreach ($autos as $auto){
+            \App\Models\Auto\Auto::create($auto);
+        }
     }
 }
