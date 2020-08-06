59113f3aa03609bf2bc67ff7709abdecfd6b12e0
Mavinx <mavinx.office@gmail.com>
2020-08-06 18:53:36 +0300

    Fix all Tests

--- tests/Feature/Admin/Tests/InvoiceTest.php
+++ tests/Feature/Admin/Tests/InvoiceTest.php
@@ -15,7 +15,7 @@ class InvoiceTest extends AdminCase
 {
     /** @test */
 
-    /*public function create_invoice_test()
+    public function create_invoice_test()
     {
         $auto_id = Auto::first()->value('id');
         $document = [];
@@ -24,25 +24,24 @@ class InvoiceTest extends AdminCase
         $response = $this->post("$this->uri/store-invoice",[
             'name_car'              => 'Mercedes',
             'auto_id'               => $auto_id,
-            'status'                => 'new',
+            'status'                => 'paid',
             'total_price'           => 10000,
             'paid_price'            => 14000,
             'outstanding_price'     => 43000,
             'total_shipping_price'           => 10000,
             'paid_shipping_price'            => 14000,
             'outstanding_shipping_price'     => 43000,
-            'status_shipping'       => 'unpaid',
+            'status_shipping'       => 'paid',
             'document'              => $document
         ], ['Authorization' => $this->getToken()]);
         $this->withoutExceptionHandling();
         $response->assertStatus(200);
-    }*/
+    }
 
     /** @test */
 
     public function get_all_invoice_test()
     {
-
         $response = $this->get("$this->uri/get-invoices",
             ['Authorization' => $this->getToken()]);
         $this->withoutExceptionHandling();
@@ -52,79 +51,80 @@ class InvoiceTest extends AdminCase
 
     /** @test */
 
-    /*public function get_invoice_by_id_test()
+    public function get_invoice_by_id_test()
     {
-        $invoice_id = Invoice::first()->value('id');
-        $this->withoutExceptionHandling();
-        $response = $this->get("$this->uri/get-invoice/$invoice_id",
+        $response = $this->get("$this->uri/get-invoice/" . $this->getTestInvoice(),
             ['Authorization' => $this->getToken()]);
+        $this->withoutExceptionHandling();
         $response->assertOk();
-    }*/
+    }
 
 
 
     /** @test */
-    /*public function update_invoice_test()
+    public function update_invoice_test()
     {
         $auto_id = Auto::first()->value('id');
-        $invoice_id = Invoice::first()->value('id');
-        $this->withoutExceptionHandling();
-        $response = $this->post("$this->uri/update-invoice/$invoice_id",[
+        $response = $this->post("$this->uri/update-invoice/" . $this->getTestInvoice(),[
             'name_car'              => 'Mercedes',
             'auto_id'               => $auto_id,
-            'status'                => 'new',
+            'status'                => 'paid',
             'total_price'           => 10000,
             'paid_price'            => 14000,
             'outstanding_price'     => 43000,
             'total_shipping_price'           => 10000,
             'paid_shipping_price'            => 14000,
             'outstanding_shipping_price'     => 43000,
-            'status_shipping'       => 'unpaid',
+            'status_shipping'       => 'paid',
         ], ['Authorization' => $this->getToken()]);
+        $this->withoutExceptionHandling();
         $response->assertStatus(200);
-    }*/
+    }
 
     /** @test */
 
-    /*public function restore_invoice_document_test()
+    public function restore_invoice_document_test()
     {
         $document = [];
         $document[0]['type'] = 'auction';
         $document[0]['file'] = UploadedFile::fake()->image('random.jpg');
         $document[1]['type'] = 'new';
         $document[1]['file'] = UploadedFile::fake()->image('random.jpg');
-        $invoice_id = Invoice::first()->value('id');
-        $this->withoutExceptionHandling();
-        $response = $this->post("$this->uri/restore-invoice-document/$invoice_id",[
+        $response = $this->post("$this->uri/restore-invoice-document/" . $this->getTestInvoice(),[
             'document'  => $document
         ],
             ['Authorization' => $this->getToken()]);
+        $this->withoutExceptionHandling();
         $response->assertOk();
 
-    }*/
+    }
 
     /** @test */
-    /*public function delete_invoice_document_test()
+    public function delete_invoice_document_test()
     {
-        $invoice = Invoice::first();
+        $invoice = Invoice::find($this->getTestInvoice());
         $documents =  implode(',',$invoice->documents()->take(5)->pluck('id')->toArray());
-        $this->withoutExceptionHandling();
         $response = $this->post("$this->uri/delete-invoice-document/$invoice->id",[
             'ids'  => $documents
         ],
             ['Authorization' => $this->getToken()]);
+        $this->withoutExceptionHandling();
         $response->assertOk();
-    }*/
+    }
 
 
     /** @test */
-    /*public function delete_invoice_test()
+    public function delete_invoice_test()
     {
-        $this->withoutExceptionHandling();
-        $invoice_id = Invoice::first()->value('id');
-        $response = $this->delete("$this->uri/delete-invoice/$invoice_id",[
+        $response = $this->delete("$this->uri/delete-invoice/" . $this->getTestInvoice(),[
         ], ['Authorization' => $this->getToken()]);
+        $this->withoutExceptionHandling();
         $response->assertOk();
 
-    }*/
+    }
+
+    public function getTestInvoice()
+    {
+        return Invoice::where('name_car', 'Mercedes')->first()->id;
+    }
 }
