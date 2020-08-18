dcb87dcfd0cc340ebf6cec7f7dae15e3440dbcd7
Mavinx <mavinx.office@gmail.com>
2020-08-18 10:57:49 +0300

    Fix

--- app/Repositories/Client/PartRepository.php
+++ app/Repositories/Client/PartRepository.php
@@ -23,7 +23,8 @@ class PartRepository implements PartContract
     public function __construct()
     {
         $catalogNumbers = Part::select('catalog_number')->distinct()->get();
-        $vinNumbers = LotInfo::whereNotNull('vin_code')->get('vin_code')->unique();
+        $vinNumbers = LotInfo::select('vin_code')->distinct()->get();
+
         $this->additional = [
             'catalog_numbers'   => $catalogNumbers,
             'vin_numbers'       => $vinNumbers,
